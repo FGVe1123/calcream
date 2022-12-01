9fb2d8ac3e43f1f29503f4be63ea690cba2419c1
Victor Flores <victor_flores.g@outlok.com>
2022-12-01 14:50:45 -0600

    primeras migraciones, modelos, creacion archivos seeder, controladores

---
+++ app/Models/Tamanio.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tamanio extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+    protected $fillable = ('tamanio');
+}
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/tailwind', function(){
+    return view('pruebatailwind');
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
